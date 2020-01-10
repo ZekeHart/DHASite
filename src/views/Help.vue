9c8e3f4dce364b5acf5b7e4419ba70f0f489bcf7
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2020-01-10 14:59:53 -0500

    add links to help page

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -4,16 +4,18 @@
 
     <h2 class='section-header'>Donate</h2>
         <ul>
-            <li class='donate-link'>ashleycanady22@yahoo.com</li>
-                <p class='donate-info'>Above is a business PayPal account operated by Ashley Canady, McDougald Terrace's resident council president. All funds go back into the community. Learn how to send money via PayPal <a target="_blank" rel="noopener" href="https://www.paypal.com/hk/smarthelp/article/how-do-i-send-a-payment-to-someone%E2%80%99s-email-address-faq1684">here.</a></p>
+            <li class='donate-link'>Give through PayPal</li>
+                <p class='donate-info'>PayPal Account: <strong>ashleycanady22@yahoo.com</strong></p>
+                <p class='donate-info'>Use the email above to send funds directly to the business PayPal account operated by Ashley Canady, McDougald Terrace's resident council president. All funds go back into the community. Learn how to send money via PayPal <a target="_blank" rel="noopener" href="https://www.paypal.com/hk/smarthelp/article/how-do-i-send-a-payment-to-someone%E2%80%99s-email-address-faq1684">here.</a></p>
             <li class='donate-link'><a href='https://www.gofundme.com/f/the-good-neighbor-fund' target="_blank" rel="noopener">The Good Neighbor Fund</a></li>
                 <p class='donate-info'>Donations to The Good Neighbor Fund are used by community organizers to increase the food security and health of those who have been displaced from McDougald Terrace. This fund is the <strong>original GoFundMe</strong> approved by Ashley.</p>
         </ul>
 
     <h2 class='section-header'>Volunteer</h2>
         <ul>
-            <li class='volunteer-info'>Visit <strong>51b Ridgeway Avenue in Durham</strong> from 10AM to 5PM to drop off donations, sort donations, or provide transportation.</li>
-            <li class='volunteer-info'>To get involved with the delivery of hot meals to displaced residents at hotels, send an e-mail to mcdougaldterracemeals@gmail.com. More information can be found <a target="_blank" rel="noopener" href='https://www.facebook.com/ZwelisKitchen/photos/a.629804900507645/1447539038734223/?type=3&theater'>here.</a></li>
+            <li class='volunteer-info'>Visit <strong><a href="https://goo.gl/maps/4BSH4osN1r76Azfs6" target="_blank" rel="noopener">51b Ridgeway Avenue in Durham</a></strong> from 10AM to 5PM to drop off donations, sort donations, or provide transportation.</li>
+            <li class='volunteer-info'>To get involved with the delivery of hot meals to displaced residents at hotels, send an e-mail to <a href="mailto:mcdougaldterracemeals@gmail.com">mcdougaldterracemeals@gmail.com</a>. More information can be found <a target="_blank" rel="noopener" href='https://www.facebook.com/ZwelisKitchen/photos/a.629804900507645/1447539038734223/?type=3&theater'>here.</a></li>
+            <li class='volunteer-info'>To stay up-to-date with information, join the Facebook group <strong><a href="https://www.facebook.com/groups/3229376280410087/?ref=br_rs" target="_blank" rel="noopener">DHA vs. Everybody</a></strong>. </li>
         </ul>
     
     <h2 class='section-header'>Constant Needs</h2>
