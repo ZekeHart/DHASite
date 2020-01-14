108193787eea2cf33de9d7c47b37904c03e42a9b
benkoval <bethanykoval@gmail.com>
2020-01-13 21:49:38 -0500

    more stuff

--- src/components/Testimony.vue
+++ src/components/Testimony.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page-container">
-      <h1 class="speaker-name">{{name}}</h1>
-      <img class="speaker-img" :src=imageUrl> 
+      <h1 class="speaker-name-t">{{name}}</h1>
+      <img class="speaker-img-t" :src=imageUrl> 
       <p class="testimony-text">{{testimonyText}}</p>
   </div>
 </template>
@@ -29,13 +29,59 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.speaker-name {
+<style lang='scss' scoped>
+.speaker-name-t {
     padding-top: 10%;
     font-size: 2rem;
+    margin: auto;
+    text-align: center;
+    padding-bottom: 5%;
 }
 
-.speaker-img {
-  max-width: 60%;
+.speaker-img-t {
+    max-width: 60%;
+    margin: auto;
+    align-self: center;
+    display: block;
+    border-radius: 50%;
+    border: #8c67ef solid 2px;
+  }
+
+  .testimony-text {
+    margin-top: 5%;
+  }
+
+
+@media (min-width: 1281px) {
+  
+  .speaker-img-t {
+      max-width: 20%;
+      margin-top: 0;
+      margin: auto;
+      align-self: center;
+      display: block;
+      border-radius: 50%;
+      border: #8c67ef solid 2px;
+    }
+  .speaker-name-t {
+      text-align: center;
+      margin-top: 5%;
+      margin-bottom: 2%;
+      font-size: 3rem;
+  }
+
+  .testimony-text {
+      margin-top: 3%;
+      font-size: 1.5rem;
+      line-height: 3rem;
+  }
+  .btn {
+      margin-bottom: 5%;
+  }
+    .speaker-name {
+      margin-top: 2%;
+      margin-bottom: 2%;
+      font-size: 1.5rem;
+  }
 }
 </style>
